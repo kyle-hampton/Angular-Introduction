894883279fe670974d113a3a96c85ff95cefd5aa
kyle-hampton <kyledr215@gmail.com>
2022-04-28 16:50:00 -0400

    Learned about arrow functions

--- hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.js
+++ hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.js
@@ -1,14 +1,8 @@
-// Type Assertions
-// Type Assertions --> explictly telling the complier the type of a varible
-// Assigns message as an 'any' datatype varible when varible is not defined
-let message;
-// assigns message to a String DataType
-message = 'abc';
-// .endsWith(STRING) works with only strings types and Not an Object with type 'any'
-// <string> explictly tells the TypeScript complier that the varible is a "string" DataType
-// option 1 for type assertion: (<string> message)
-// option 2 for type assertion: (message as string)
-// option 1
-let endsWithC = message.endsWith('c');
-// option 2
-let alternativeWay = message.endsWith('c');
+// arrow-functions in TypeScript
+// Arrow Functions allow for shorter and cleaner fucntion code
+// Normal Function
+var log = function (message) {
+    console.log(message);
+};
+// Arrow Function
+var doLog = function (message) { return console.log(message); };
--- hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.ts
+++ hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.ts
@@ -1,20 +1,10 @@
-// Type Assertions
-// Type Assertions --> explictly telling the complier the type of a varible
+// arrow-functions in TypeScript
+// Arrow Functions allow for shorter and cleaner fucntion code
 
-// Assigns message as an 'any' datatype varible when varible is not defined
-let message;
+// Normal Function
+let log = function(message){
+  console.log(message);
+}
 
-// assigns message to a String DataType
-message = 'abc';
-
-// .endsWith(STRING) works with only strings types and Not an Object with type 'any'
-// <string> explictly tells the TypeScript complier that the varible is a "string" DataType
-
-// option 1 for type assertion: (<string> message)
-// option 2 for type assertion: (message as string)
-
-// option 1
-let endsWithC = (<string>message).endsWith('c');
-
-// option 2
-let alternativeWay = (message as string).endsWith('c');
+// Arrow Function
+let doLog = (message) => console.log(message)
