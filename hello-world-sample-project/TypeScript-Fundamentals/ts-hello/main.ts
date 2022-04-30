58af6f7225e711e09dfa105e6db900f8e4c722d9
kyle-hampton <kyledr215@gmail.com>
2022-04-30 12:29:15 -0400

    Learned about access modifiers

--- hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.ts
+++ hello-world-sample-project/TypeScript-Fundamentals/ts-hello/main.ts
@@ -7,8 +7,15 @@
 //This is a Class
 class Point {
     // Properties
-    x: number;
-    y: number;
+    // Access Modifers (public, private, protected)
+    // Access Modifiers --> Are key words that add you can add to a memeber of a class to control its Access from the outside
+    private x: number;
+    private y: number;
+
+    constructor(x?: number, y?: number){
+      this.x = x;
+      this.y = y;
+    }
 
     // Methods
     draw(){
@@ -17,14 +24,11 @@ class Point {
     }
 }
 
-
  /* To create an instance of a class you have to allocate memory space this is
  done through the "new ClassNameHere()" */
  // Classes create new instances of Objects
 
 // This is an Objects --> new Point();
 // this Object is assigned to a varible named 'point'
-let point = new Point();
-point.x = 1;
-point.y = 2;
+let point = new Point(1 ,2);
 point.draw();
